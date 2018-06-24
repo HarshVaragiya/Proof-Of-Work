17c0b6caf75b94cc2223cb170960c60c717591b3
Harsh Varagiya <32223129+HarshVaragiya@users.noreply.github.com>
2018-06-25 01:19:32 +0530

    Update Miner.py

--- Miner.py
+++ Miner.py
@@ -1,32 +1,36 @@
-from Crypto import Random
-import hashlib
-import time
-import threading
-from Difficulty import System
+# Author : xtreme.research@gmail.com
+# Requires PyCrypto/PyCryptoDome for Random Number Generator! NOT NECESSARY, just for more secure random numbers and more
+# Even Probability Distibution For Better Performance 
 
-a = System(256,1)
-miner_count = 500
-block_count = 0
-hashes_count = 0
-runtime = 120
+from Crypto import Random                         # From Pycryptodome
+import hashlib                                    # For SHA256
+import time                                       # For Runtime Analysis 
+import threading                                  # Miners Run on Several Threads
+from Difficulty import System                     # Import basic System Class for checking proof-of-work
 
-start_time = time.time()
-last_time = start_time
+a = System(256,1)                                 # Object to check Proof-Of-Work 256 Bits , intitial Difficulty = 1 
+miner_count = 500                                 # Total Number of Miner Threads 
+block_count = 0                                   # Total Blocks Counted  
+hashes_count = 0                                  # Total Hashes Calculated
+runtime = 120                                     # For Burnout, Specify time 
 
-class Miner(threading.Thread):
+start_time = time.time()                          # Log start time 
+last_time = start_time                            # time when last block was solved
+
+class Miner(threading.Thread):                    # Threaded
     def __init__(self,SYSTEM_TARGET_OBJECT,thread_id=0,timer_delay=0.0,random_byte_length=256):
-        threading.Thread.__init__(self)
-        self.thread_id = thread_id
-        #self.daemon = True
-        self.time_delay = timer_delay
-        self.random_byte_length = random_byte_length
-        self.System_Obj = SYSTEM_TARGET_OBJECT
-        self.stay_alive = True
+        threading.Thread.__init__(self)          
+        self.thread_id = thread_id                # Thread ID
+        self.daemon = True                        # So threads exit when program exits 
+        self.time_delay = timer_delay             # Time Delay to demonstrate lower hash rates
+        self.random_byte_length = random_byte_length # random byte length for pycryptodome random bytes generator
+        self.System_Obj = SYSTEM_TARGET_OBJECT    # Object to check proof of work 
+        self.stay_alive = True                    # Thread Killing Thingy
     
-    def get_random_int(self,bytess):                                           # Change Random Function if required
-        return Random.get_random_bytes(bytess)
+    def get_random_int(self,bytess):              # Change Random Function if required
+        return Random.get_random_bytes(bytess)    # this is cryptographically Secure so better probability distibution
     
-    def generate_hash(self):
+    def generate_hash(self):                      # get int of the hash of the random number 
         self.x = self.get_random_int(self.random_byte_length)
         self.hash = hashlib.sha256(self.x).hexdigest()
         self.ret = int(self.hash,16)
@@ -36,9 +40,10 @@ class Miner(threading.Thread):
     def run(self):
         global hashes_count,last_time
         while self.stay_alive == True:
+            time.sleep(self.time_delay)         # To demonstrate slower hash rates
             self.h = self.generate_hash()
             hashes_count +=1
-            if(self.System_Obj.check_hash(self.h) == True):
+            if(self.System_Obj.check_hash(self.h) == True):        # if block is solved 
                 global block_count
                 block_count +=1
                 print("Block {} Solved By Miner Thread : {} ".format(block_count,self.thread_id))
@@ -54,9 +59,10 @@ class Miner(threading.Thread):
 
 miners = []
 
+# Start all miners 
 for i in range(miner_count):
-    x = Miner(a,i)
-    x.start()
+    x = Miner(a,i)               
+    x.start()                       
 
 time.sleep(runtime)
 
@@ -65,8 +71,7 @@ for miner in miners:
 
 stop_time = time.time()
 
-if(stop_time<start_time):
-    stop_time +=60
+# Stats and Math 
 
 time_elapsed = stop_time - start_time
 hash_rate = (hashes_count/time_elapsed) / 1000
