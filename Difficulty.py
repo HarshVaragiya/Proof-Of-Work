f9d7ea9bf10b8552c65e116048c199a8addd4e2d
Harsh Varagiya <32223129+HarshVaragiya@users.noreply.github.com>
2018-06-25 01:10:01 +0530

    Update Difficulty.py

--- Difficulty.py
+++ Difficulty.py
@@ -1,26 +1,30 @@
+# Author : xtreme.research@gmail.com
+# Class Definition of CryptoSystem which checks for given hash for proof-of-work
+
+# Basic Function that Exponentially Increases the difficulty, so Time to Solve increases Exponentially 
 def Exponential(init_bit_length):
     return init_bit_length+1
 
 class System:
     
-    def __init__(self,BIT_LEN=256,INITIAL_TARGET_BITS=1,DIFFICULTY_UPDATE_FUNCTION=Exponential):
-        self.bit_len = BIT_LEN
-        self.bit_modifier = INITIAL_TARGET_BITS
-        self.update_func = DIFFICULTY_UPDATE_FUNCTION
-        self.calc_target()
+    def __init__(self,BIT_LEN=256,INITIAL_TARGET_BITS=1,DIFFICULTY_UPDATE_FUNCTION=Exponential):     # Init 
+        self.bit_len = BIT_LEN                                                 # SHA256 so 256 bits
+        self.bit_modifier = INITIAL_TARGET_BITS                                # Staring difficulty 
+        self.update_func = DIFFICULTY_UPDATE_FUNCTION                          # When Block is Solved, Update Target
+        self.calc_target()                                                     # Calculate Target Value 
     
     def calc_target(self):
-        self.target = (2**(256-self.bit_modifier)) 
-
-    def show_difficulty(self):
-        return self.bit_modifier
+        self.target = (2**(self.bit_len-self.bit_modifier))                    # 2 ^ (256-difficulty) so smaller values for 
+                                                                               # greater difficulty
+    def show_difficulty(self):                                                 
+        return self.bit_modifier                                               # show difficulty in bits
     
-    def on_solve(self):
-        self.bit_modifier = self.update_func(self.bit_modifier)
+    def on_solve(self):                                                        # when block is solved, update
+        self.bit_modifier = self.update_func(self.bit_modifier)                # and calculate target again
         self.calc_target()
 
-    def check_hash(self,chk_hash):
-        if chk_hash<self.target:
+    def check_hash(self,chk_hash):                                             # basic function to check given hash
+        if chk_hash<self.target:                                               # against target value 
             self.on_solve()
             return True
         else:
