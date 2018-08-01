1996308d7d12e273a77b53e60c6a19a634a72f05
Harsh Varagiya <32223129+HarshVaragiya@users.noreply.github.com>
2018-08-01 19:25:34 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,19 +3,17 @@
 # Even Probability Distibution For Better Performance 
 
 from Crypto import Random                         # From Pycryptodome
-import hashlib                                    # For SHA256
 import time                                       # For Runtime Analysis 
-import threading                                  # Miners Run on Several Threads
 from Difficulty import System                     # Import basic System Class for checking proof-of-work
 from Miner import Miner
 
 Example_System = System()                         # Object to check Proof-Of-Work 256 Bits , intitial Difficulty = 1 
 miner_count = 500                                 # Total Number of Miner Threads 
-RUNTIME = (120)                                    # For Burnout, Specify time 
+RUNTIME = (120)                                   # For Burnout, Specify time 
 start_time = time.time()
 miners = []
-# Start all miners 
 
+# Start all miners 
 for i in range(miner_count):
     x = Miner(Example_System,i)               
     x.start()    
@@ -27,10 +25,14 @@ block_count = Example_System.get_block_count()
 
 for miner in miners:
     miner.halt_thread()
-# Stats and Math
+
+    # Stats and Math
 
 time_elapsed = stop_time - start_time
 hash_rate = (hashes_count/time_elapsed) / 1000
+
+#Pretty Print Stats
+
 print("-"*60)
 print("Total Miner-Threads     : {} ".format(miner_count))
 print("Total Blocks Solved     : {} ".format(block_count))
@@ -38,3 +40,7 @@ print("Total Hashes Calculated : {} ".format(hashes_count))
 print("Total Time Elapsed      : {} Seconds".format(stop_time-start_time))
 print("HashRate for the Run    : {} KHashes/Sec".format(hash_rate))
 print("-"*60)
+
+exit()                           
+
+# Just in case :p 
